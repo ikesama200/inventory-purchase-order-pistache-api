5fc8b9012a0e93d88a3a7707cb71769be36b1076
ikesama200 <y_ikemoto@shikisai.jp.net>
2025-09-01 02:26:11 +0900

    select用のSQLの共通化は取りやめて商品マスタ全件取得の処理を追加

--- main.cpp
+++ main.cpp
@@ -102,6 +102,28 @@ public:
         }
     }
 
+    // 商品マスタ全件取得
+    void getProducts(const Rest::Request&, Http::ResponseWriter response) {
+        try {
+            std::string query = loadSqlQuery("sql/select_products.sql");
+            pqxx::work txn(conn);
+            pqxx::result r = txn.exec(query);
+            // txn.commit(); // select処理なのでコメントアウト
+
+            json result = json::array();
+            for (auto row : r) {
+                json obj;
+                for (auto field : row) {
+                    obj[field.name()] = field.c_str() ? field.c_str() : "";
+                }
+                result.push_back(obj);
+            }
+            response.send(Http::Code::Ok, result.dump(), MIME(Application, Json));
+        } catch (const std::exception& e) {
+            response.send(Http::Code::Internal_Server_Error, e.what());
+        }
+    }
+
     // INSERT
     void handleInsert(const Rest::Request& request, Http::ResponseWriter response) {
         try {
@@ -209,7 +231,9 @@ int main() {
 
     // テーブル情報取得API
     Rest::Routes::Get(router, "/get-category", Rest::Routes::bind(&ApiHandler::getCategory, &handler));
+    Rest::Routes::Get(router, "/get-product", Rest::Routes::bind(&ApiHandler::getProducts, &handler));
 
+    
     // テーブル書き込みAPI
 
     // samapleAPI
